eea4066f3e317fbb568271f488153b821b295c8e
권미리 <miri@gwonmiliui-MacBookPro.local>
2025-03-27 14:05:43 +0900

    feat: LessonPackage 및 UserLesoon 엔티티 추가

---
+++ src/main/java/ringle/tutoring/domain/lesson/UserLesson.java
@@ -0,0 +1,48 @@
+package ringle.tutoring.domain.lesson;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import ringle.tutoring.domain.common.entity.BaseTimeEntity;
+import ringle.tutoring.domain.common.enums.Active;
+import ringle.tutoring.domain.user.User;
+
+@Entity
+@Table(name = "user_lessons")
+public class UserLesson extends BaseTimeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_lesson_id")
+  private long userLessonId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "lesson_id")
+  private LessonPackage lessonPackage;
+
+  @Column(name = "user_lesson_purchased_at", nullable = false)
+  private LocalDateTime userLessonPurchasedAt;
+
+  @Column(name = "user_lesson_start_date", nullable = false)
+  private LocalDateTime userLessonStartDate;
+
+  @Column(name = "user_lesson_end_date", nullable = false)
+  private LocalDateTime userLessonEndDate;
+
+  @Column(name = "user_lesson_remained", nullable = false)
+  private int userLessonRemained = 0;
+
+  @Column(name = "user_lesson_status", nullable = false)
+  private Active userLessonStatus = Active.active;
+
+}
